8e4b5bb144da4b4774a5cc511319b909a29585bc
dnjs0236 <dnjs0236@gmail.com>
2023-03-08 09:02:37 +0900

    feat : create story

--- frontend/frontproject/src/components/storyCreate/genreList.tsx
+++ frontend/frontproject/src/components/storyCreate/genreList.tsx
@@ -1,43 +1,5 @@
 import { useState, useEffect } from 'react'
 import '../../assets/css/storyCreatePageStyle.css'
 export default function ImageUpload() {
-  const [genre, setGenre] = useState('')
-
-  const clickGenre = (e: any) => {
-    e.target.classList.add('active')
-    setGenre(e.target.value)
-  }
-
-  const items = ['재미', '슬픔', '공포', '로맨스']
-
-  return (
-    <>
-      <div className="container">
-        {items.map((item, idx) => {
-          let id = 'genreBtn-' + (idx + 1)
-          console.log(id)
-          return (
-            <>
-              <input
-                id={id}
-                type="radio"
-                name="gerne"
-                value={items[idx]}
-                onChange={clickGenre}
-              ></input>{' '}
-              <label
-                className={
-                  'genre-label' + (items[idx] == genre ? '-active' : '')
-                }
-                htmlFor={id}
-              >
-                {items[idx]}{' '}
-              </label>
-            </>
-          )
-        })}
-      </div>{' '}
-      <button className="createBtn">이야기 만들기</button>
-    </>
-  )
+  return <></>
 }
