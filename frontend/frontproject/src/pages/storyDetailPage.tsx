e5fe3510efea1737795d7a2500c9306ec4d110fb
dnjs0236 <dnjs0236@gmail.com>
2023-03-10 16:12:59 +0900

    [FEAT] #80 이야기 상세 페이지

---
+++ frontend/frontproject/src/pages/storyDetailPage.tsx
@@ -0,0 +1,8 @@
+export default function StoryDetailPage() {
+  return (
+    <>
+      <div>1</div>
+      <div>2</div>
+    </>
+  )
+}
--- frontend/frontproject/src/routes/RoutesSetup.tsx
+++ frontend/frontproject/src/routes/RoutesSetup.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Main from '../components/main/main'
 import StoryCreatePage from '../pages/storyCreatePage'
 import StoryResultPage from '../pages/storyResultPage'
+import StoryDetailPage from '../pages/storyDetailPage'
 import LibraryPage from '../pages/LibraryPage'
 import Loading from '../components/storyCreate/loading'
 export default function RoutesSetup() {
@@ -11,6 +12,7 @@ export default function RoutesSetup() {
       <Route path="/storyCreatePage" element={<StoryCreatePage />}></Route>
       <Route path="/storyResult" element={<StoryResultPage />}></Route>
       <Route path="/library" element={<LibraryPage />}></Route>
+      <Route path="/storyDetail" element={<StoryDetailPage />}></Route>
       <Route path="/Loading" element={<Loading />}></Route>
     </Routes>
   )
