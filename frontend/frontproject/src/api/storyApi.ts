5423b51751011658c5c48e8904240570e7ddebb2
Ajin <lany1515@naver.com>
2023-03-16 12:15:46 +0900

    [EDIT] commit error 수정

--- frontend/frontproject/src/api/storyApi.ts
+++ frontend/frontproject/src/api/storyApi.ts
@@ -58,27 +58,3 @@ export async function deleteStory(id: number) {
 	const response: AxiosResponse = await axios.delete(`/api/story/${id}`);
 	return response;
 }
-
-// 음성 생성
-export async function createVoice(content: string, genre: string) {
-  const response: AxiosResponse = await axios.post(`${BASE_URL}/story/voice`, {
-    content: content,
-    genre: genre,
-  })
-  return response
-}
-
-// 이야기 번역
-export async function translateStory(content: string) {
-  const response: AxiosResponse = await axios.post(
-    `${BASE_URL}/story/translate`,
-    { content: content },
-  )
-  return response
-}
-
-// 이야기 삭제
-export async function deleteStory(id: number) {
-  const response: AxiosResponse = await axios.delete(`/api/story/${id}`)
-  return response
-}
