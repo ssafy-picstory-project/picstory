166d6b4acce2bb6f0966e7a85e9e2f04f80fe37c
최성민 <et_b612@naver.com>
2023-03-20 16:37:54 +0900

    이메일, 닉네임 중복검사

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,7 +4,9 @@ from .views import (
     verify_email ,
     login,send_email_verify_code,
     token_refresh,
-    test
+    test,
+    check_duplicate_email,
+    check_duplicate_nickname
 )
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -18,6 +20,8 @@ urlpatterns = [
     path('resend/code/',send_email_verify_code,name='send_email_verify_code'),
     path('verify/email/', verify_email, name='verify_email'),
     path('login/', login, name='login'),
+    path('check/duplicate/email/', check_duplicate_email, name='check_duplicate_email'),
+    path('check/duplicate/nickname/', check_duplicate_nickname, name='check_duplicate_nickname'),
 
 
     path('token/refresh/', token_refresh, name='refresh_token'),
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -77,6 +77,27 @@ def signup(request):
         return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
     
 
+
+@csrf_exempt
+def check_duplicate_email(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        email = data.get('email')
+        if Member.objects.filter(email=email).exists():
+            return JsonResponse({'result': True})
+        else:
+            return JsonResponse({'result': False})
+        
+@csrf_exempt
+def check_duplicate_nickname(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        nickname = data.get('nickname')
+        if Member.objects.filter(nickname=nickname).exists():
+            return JsonResponse({'result': True})
+        else:
+            return JsonResponse({'result': False})
+
 @csrf_exempt
 def send_email_verify_code(request):
     """이메일 인증코드 재전송
