88bc457b33a55bf8b0cb1eddfb7b3980a467d696
Ajin <lany1515@naver.com>
2023-03-21 13:01:22 +0900

    [EDIT] #129 오디오결과 테스트
    - 오디오 불러오기, 저장 성공
    
    - notfound, layout 추가
    
    - 라우터 수정

--- frontend/frontproject/src/routes/RoutesSetup.tsx
+++ frontend/frontproject/src/routes/RoutesSetup.tsx
@@ -5,16 +5,21 @@ import StoryResultPage from '../pages/storyResultPage'
 import StoryDetailPage from '../pages/storyDetailPage'
 import LibraryPage from '../pages/LibraryPage'
 import Loading from '../components/storyCreate/loading'
+import NotFound from '../pages/NotFound'
+import Layout from '../components/main/Layout'
 
 export default function RoutesSetup() {
   return (
     <Routes>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/storyCreatePage" element={<StoryCreatePage />}></Route>
-      <Route path="/storyResult" element={<StoryResultPage />}></Route>
-      <Route path="/library" element={<LibraryPage />}></Route>
-      <Route path="/storyDetail/:id" element={<StoryDetailPage />}></Route>
-      <Route path="/Loading" element={<Loading />}></Route>
+      <Route element={<Layout  />}>
+        <Route path="/" element={<Main />}/>
+        <Route path="/storyCreatePage" element={<StoryCreatePage />}/>
+        <Route path="/storyResult" element={<StoryResultPage />}/>
+        <Route path="/library" element={<LibraryPage />}/>
+        <Route path="/storyDetail/:id" element={<StoryDetailPage />}/>
+        <Route path="/Loading" element={<Loading />}/>
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
