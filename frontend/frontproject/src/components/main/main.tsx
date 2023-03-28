c60c518a66b3c368e949b78fa5814f33a26690bf
dnjs0236 <dnjs0236@gmail.com>
2023-03-28 14:32:51 +0900

    [EDIT] #272 메인화면 디자인 수정

--- frontend/frontproject/src/components/main/main.tsx
+++ frontend/frontproject/src/components/main/main.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import styles from '../../assets/css/main.module.css'
 import { useRecoilState } from 'recoil'
 import { colorAtom } from '../../atoms'
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Main() {
   const navigation = useNavigate()
@@ -29,29 +29,117 @@ export default function Main() {
     }
   }
 
+  const outerDivRef = useRef<HTMLDivElement>(null)
+  const [scrollIndex, setScrollIndex] = useState(1)
+  const DIVIDER_HEIGHT = 5
+
   useEffect(() => {
-    setColor(colorList[idx])
-  })
+    const wheelHandler = (e: any) => {
+      e.preventDefault()
+      const { deltaY } = e
+      const { scrollTop }: any = outerDivRef.current // 스크롤 위쪽 끝부분 위치
+      const pageHeight = window.innerHeight // 화면 세로길이, 100vh와 같습니다.
+
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          console.log('현재 1페이지, down')
+          outerDivRef.current?.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: 'smooth',
+          })
+          setScrollIndex(2)
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log('현재 2페이지, down')
+          outerDivRef.current?.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: 'smooth',
+          })
+          setScrollIndex(3)
+        } else {
+          // 현재 3페이지
+          console.log('현재 3페이지, down')
+          outerDivRef.current?.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: 'smooth',
+          })
+          setScrollIndex(3)
+        }
+      } else {
+        // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          console.log('현재 1페이지, up')
+          outerDivRef.current?.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          })
+          setScrollIndex(1)
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log('현재 2페이지, up')
+          outerDivRef.current?.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          })
+          setScrollIndex(1)
+        } else {
+          // 현재 3페이지
+          console.log('현재 3페이지, up')
+          outerDivRef.current?.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: 'smooth',
+          })
+          setScrollIndex(2)
+        }
+      }
+    }
+    const outerDivRefCurrent = outerDivRef.current
+    outerDivRefCurrent?.addEventListener('wheel', wheelHandler)
+    return () => {
+      outerDivRefCurrent?.removeEventListener('wheel', wheelHandler)
+    }
+  }, [])
 
   return (
-    <div className={`${styles['container']} ${styles[color]}`}>
-      <div className={styles.picstory}>
-        <h1>
-          <span>p</span>
-          <span>i</span>
-          <span>c</span>
-          <span>s</span>
-          <span>t</span>
-          <span>o</span>
-          <span>r</span>
-          <span>y</span>
-        </h1>
-      </div>
-      <div className={styles.clear}></div>
+    <div ref={outerDivRef} className={styles.outer}>
+      <div className={styles.inner}>
+        <div className={styles.section1}>
+          <div className={styles.picstory}>
+            <h1>
+              <span>p</span>
+              <span>i</span>
+              <span>c</span>
+              <span>s</span>
+              <span>t</span>
+              <span>o</span>
+              <span>r</span>
+              <span>y</span>
+            </h1>
+          </div>
+          <div className={styles.clear}></div>
 
-      <button onClick={handleTry} className={styles.btn1}>
-        TRY
-      </button>
+          <button onClick={handleTry} className={styles.btn1}>
+            TRY
+          </button>
+        </div>
+      </div>
+      <div className={styles.divider1}></div>
+      <div className={styles.inner}>
+        <div className={styles.section2}></div>
+      </div>
+      <div className={styles.divider2}></div>
+      <div className={styles.inner}>
+        <div className={styles.section3}></div>
+      </div>
     </div>
   )
 }
