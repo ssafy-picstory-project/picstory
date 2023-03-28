d2164377988b6d18b2b10d252c300e489e56e8cb
ajin <lany1515@naver.com>
2023-03-28 22:08:57 +0900

    [STYLE]#270 회원가입 초기디자인

--- frontend/frontproject/src/components/main/TheHeader.tsx
+++ frontend/frontproject/src/components/main/TheHeader.tsx
@@ -6,56 +6,55 @@ import menuIcon from "../../assets/menu.png";
 import Swal from "sweetalert2";
 import { deleteUser } from "../../api/userAPI";
 
+// 회원정보
+export const myInfo = () => {
+	const userEmail = sessionStorage.getItem("userEmail");
+	const userNick = sessionStorage.getItem("userNick");
+	Swal.fire({
+		title: `${userNick}`,
+		text: `${userEmail}`,
+		showCancelButton: true,
+		confirmButtonColor: "#3085d6",
+		cancelButtonColor: "#d33",
+		confirmButtonText: "회원탈퇴",
+	}).then((result) => {
+		if (result.isConfirmed) {
+			Swal.fire({
+				title: "탈퇴하시겠습니까?",
+				text: "이야기와 단어장 정보가 모두 사라집니다!",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#3085d6",
+				cancelButtonColor: "#d33",
+				confirmButtonText: "회원 탈퇴",
+			}).then(async (result) => {
+				if (result.isConfirmed) {
+					try {
+						const response = await deleteUser();
+						if (response.data.status === 200) {
+							Swal.fire(
+								"탈퇴성공!",
+								"Your file has been deleted.",
+								"success"
+							);
+						}
+					} catch (error) {
+						Swal.fire({
+							icon: "error",
+							title: "Oops...",
+							text: "탈퇴실패했습니다.",
+						});
+					}
+				}
+			});
+		}
+	});
+};
+
 const TheHeader = () => {
 	const navigation = useNavigate();
 	const color = useRecoilValue(colorAtom);
 	const setMenu = useSetRecoilState(menuState);
-
-	const myInfo = () => {
-		const userEmail = sessionStorage.getItem("userEmail");
-		const userNick = sessionStorage.getItem("userNick");
-
-		Swal.fire({
-			title: `${userNick}`,
-			text: `${userEmail}`,
-			icon: "question",
-			showCancelButton: true,
-			confirmButtonColor: "#3085d6",
-			cancelButtonColor: "#d33",
-			confirmButtonText: "회원탈퇴",
-		}).then((result) => {
-			if (result.isConfirmed) {
-				Swal.fire({
-					title: "탈퇴하시겠습니까?",
-					text: "이야기와 단어장 정보가 모두 사라집니다!",
-					icon: "warning",
-					showCancelButton: true,
-					confirmButtonColor: "#3085d6",
-					cancelButtonColor: "#d33",
-					confirmButtonText: "회원 탈퇴",
-				}).then(async (result) => {
-					if (result.isConfirmed) {
-						try {
-							const response = await deleteUser();
-							if (response.data.status === 200) {
-								Swal.fire(
-									"탈퇴성공!",
-									"Your file has been deleted.",
-									"success"
-								);
-							}
-						} catch (error) {
-							Swal.fire({
-								icon: "error",
-								title: "Oops...",
-								text: "탈퇴실패했습니다.",
-							});
-						}
-					}
-				});
-			}
-		});
-	};
 
 	// 로그아웃
 	const logout = () => {
