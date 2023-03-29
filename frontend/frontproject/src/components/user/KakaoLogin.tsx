f403dcefd739f06967a6e7f06376b4d135a5f03d
soshui <615ksh@gmail.com>
2023-03-29 14:50:12 +0900

    [FIX] 카카오로그인 코드 수정

--- frontend/frontproject/src/components/user/KakaoLogin.tsx
+++ frontend/frontproject/src/components/user/KakaoLogin.tsx
@@ -3,7 +3,7 @@ import queryString from "query-string";
 import styles from "../../assets/css/main.module.css";
 import { kakao } from "../../api/userAPI";
 import Swal from "sweetalert2";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 
 function KakaoLogin() {
 	const navigate = useNavigate();
@@ -11,77 +11,57 @@ function KakaoLogin() {
 	const queryParams = queryString.parse(location.search);
 
 	let code: any = queryParams.code;
-	console.log("code: ", code);
-	console.log("code: ", typeof code);
 
-	const onCodeSend = async (code: string) => {
-		try {
-			const res = await kakao(code);
-			console.log("res 카카오로그인", res);
-			// 로그인 요청 성공 시 토큰과 유저정보 저장 후 페이지 이동
-			const result = res.data;
-			if (res.status === 200) {
-				// sessionStorage에 이메일과 닉네임 저장
-				sessionStorage.setItem("userEmail", result.email);
-				sessionStorage.setItem("userNick", result.nickname);
-				// localStorage에 토큰 저장
-				localStorage.setItem(
-					"access_token",
-					JSON.stringify(result.access_token)
-				);
-				localStorage.setItem(
-					"refresh_token",
-					JSON.stringify(result.refresh_token)
-				);
-				//mainPage로 이동하기
-				const Toast = Swal.mixin({
-					toast: true,
-					position: "top-end",
-					showConfirmButton: false,
-					timer: 3000,
-					timerProgressBar: true,
-					didOpen: (toast) => {
-						toast.addEventListener("mouseenter", Swal.stopTimer);
-						toast.addEventListener("mouseleave", Swal.resumeTimer);
-					},
-				});
+	const onCodeSend = useCallback(
+		async (code: string) => {
+			try {
+				const res = await kakao(code);
+				console.log("res 카카오로그인", res);
 
-				Toast.fire({
-					icon: "success",
-					title: `${result.nickname}님 안녕하세요!`,
-				});
+				// 로그인 요청 성공 시 토큰과 유저정보 저장 후 페이지 이동
+				const result = res.data;
+				if (res.status === 200) {
+					// sessionStorage에 이메일과 닉네임 저장
+					sessionStorage.setItem("userEmail", result.email);
+					sessionStorage.setItem("userNick", result.nickname);
+					// localStorage에 토큰 저장
+					localStorage.setItem(
+						"access_token",
+						JSON.stringify(result.access_token)
+					);
+					localStorage.setItem(
+						"refresh_token",
+						JSON.stringify(result.refresh_token)
+					);
+					//mainPage로 이동하기
+					const Toast = Swal.mixin({
+						toast: true,
+						position: "top-end",
+						showConfirmButton: false,
+						timer: 3000,
+						timerProgressBar: true,
+						didOpen: (toast) => {
+							toast.addEventListener("mouseenter", Swal.stopTimer);
+							toast.addEventListener("mouseleave", Swal.resumeTimer);
+						},
+					});
 
-				// navigate("/");
+					Toast.fire({
+						icon: "success",
+						title: `${result.nickname}님 안녕하세요!`,
+					});
+				}
+			} catch (error: any) {
+				navigate("/login");
+				console.log(error);
 			}
-		} catch (error: any) {
-			navigate("/login");
-			// Swal.fire({
-			// 	icon: "error",
-			// 	title: "Oops...",
-			// 	text: "카카오 로그인 실패! ",
-			// 	confirmButtonText: "RETRY",
-			// 	allowOutsideClick: false, //모달 밖에서는 클릭 안됨
-			// }).then((result) => {
-			// 	if (result.isConfirmed) {
-			// 		//실패했을 때, 다시 카카오로그인 페이지로 이동함
-			// 		handleClick();
-			// 	} else {
-			// 	}
-			// });
-			console.log(error);
-		}
-	};
-	//카카오 로그인 페이지로 이동
-	const handleClick = () => {
-		navigate("/login");
-		// window.location.href =
-		// 	"http://192.168.100.140:8000/api/accounts/kakao/login/";
-	};
+		},
+		[navigate]
+	);
 
 	useEffect(() => {
-		console.log("useEffect code: ", code);
 		onCodeSend(code);
-	}, []);
+	}, [code, onCodeSend]);
 
 	return (
 		<>
@@ -93,12 +73,14 @@ function KakaoLogin() {
 			>
 				<div className={styles.picstory}>
 					<h1>
-						<span>회</span>
-						<span>원</span>
-						<span>가</span>
-						<span>입</span>
-						<span>중</span>
-						<span>~</span>
+						<span>카</span>
+						<span>카</span>
+						<span>오</span>
+						<span>로</span>
+						<span>그</span>
+						<span>인</span>
+						<span>완</span>
+						<span>료</span>
 						<span>😎</span>
 					</h1>
 				</div>
