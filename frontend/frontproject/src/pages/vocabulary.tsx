de3cdcd846189a2a6c04f223dd5a5573f8862454
ajin <lany1515@naver.com>
2023-04-03 23:35:50 +0900

    [style] 단어장 오류 수정

--- frontend/frontproject/src/pages/vocabulary.tsx
+++ frontend/frontproject/src/pages/vocabulary.tsx
@@ -34,6 +34,7 @@ export default function Vocabulary() {
 				const response = await getWordList(isSortTime ? "" : "alpha", page);
 
 				const item = response.data.results;
+
 				setCount(response.data.count);
 				setPageCount(Math.floor(response.data.count / 10) + 1);
 
@@ -79,7 +80,7 @@ export default function Vocabulary() {
 		<div className={styles.container}>
 			<div className={styles.title}>WORD</div>
 			<div className={styles.btns}>
-				<div>TOTAL {count}</div>
+				<div className={styles.total}>TOTAL {count}</div>
 				<button className={styles.sortBtn} onClick={click}>
 					<img src={sortIcon} alt='' width={30}></img>
 					{isSortTime ? "ABC" : "TIME"}
@@ -96,147 +97,62 @@ export default function Vocabulary() {
 
 				<div className={styles.container2}>
 					{isSortTime ? (
-						<table className={styles.word_table}>
-							<td>
-								<div className={styles.list}>
-									{wordListTime[0] ? wordListTime[0].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[1] ? wordListTime[1].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[2] ? wordListTime[2].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[3] ? wordListTime[3].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[4] ? wordListTime[4].word : <br />}
-								</div>
-							</td>
-							<td className={styles.col}>
-								<div className={styles.list}>
-									{wordListTime[0] ? wordListTime[0].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[1] ? wordListTime[1].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[2] ? wordListTime[2].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[3] ? wordListTime[3].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[4] ? wordListTime[4].mean : <br />}
-								</div>
-							</td>
-							<td>
-								<div className={styles.list}>
-									{wordListTime[5] ? wordListTime[5].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[6] ? wordListTime[6].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[7] ? wordListTime[7].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[8] ? wordListTime[8].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[9] ? wordListTime[9].word : <br />}
-								</div>
-							</td>
-							<td>
-								<div className={styles.list}>
-									{wordListTime[5] ? wordListTime[5].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[6] ? wordListTime[6].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[7] ? wordListTime[7].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[8] ? wordListTime[8].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListTime[9] ? wordListTime[9].mean : <br />}
-								</div>
-							</td>
-						</table>
+						<>
+							<table className={styles.firstTable}>
+								<tbody>
+									{wordListTime.slice(0, 5).map((item, i) => {
+										return (
+											<tr key={i}>
+												<td>{item && item.word}</td>
+												<td>{item && item.mean}</td>
+											</tr>
+										)
+									})}
+								</tbody>
+							</table>
+							<br />
+							<table>
+								<tbody>
+									{wordListTime.slice(5, 10).map((item, i) => {
+										return (
+											<tr key={i}>
+												<td>{item && item.word}</td>
+												<td>{item && item.mean}</td>
+											</tr>
+										)
+									})}
+								</tbody>
+							</table>
+						</>
+
 					) : (
-						<table className={styles.word_table}>
-							<td>
-								<div className={styles.list}>
-									{wordListAlpha[0] ? wordListAlpha[0].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[1] ? wordListAlpha[1].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[2] ? wordListAlpha[2].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[3] ? wordListAlpha[3].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[4] ? wordListAlpha[4].word : <br />}
-								</div>
-							</td>
-							<td className={styles.col}>
-								<div className={styles.list}>
-									{wordListAlpha[0] ? wordListAlpha[0].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[1] ? wordListAlpha[1].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[2] ? wordListAlpha[2].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[3] ? wordListAlpha[3].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[4] ? wordListAlpha[4].mean : <br />}
-								</div>
-							</td>
-							<td>
-								<div className={styles.list}>
-									{wordListAlpha[5] ? wordListAlpha[5].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[6] ? wordListAlpha[6].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[7] ? wordListAlpha[7].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[8] ? wordListAlpha[8].word : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[9] ? wordListAlpha[9].word : <br />}
-								</div>
-							</td>
-							<td>
-								<div className={styles.list}>
-									{wordListAlpha[5] ? wordListAlpha[5].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[6] ? wordListAlpha[6].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[7] ? wordListAlpha[7].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[8] ? wordListAlpha[8].mean : <br />}
-								</div>
-								<div className={styles.list}>
-									{wordListAlpha[9] ? wordListAlpha[9].mean : <br />}
-								</div>
-							</td>
-						</table>
+						<>
+							<table className={styles.firstTable}>
+								<tbody>
+									{wordListAlpha.slice(0, 5).map((item, i) => {
+										return (
+											<tr key={i}>
+												<td>{item && item.word}</td>
+												<td>{item && item.mean}</td>
+											</tr>
+										)
+									})}
+								</tbody>
+							</table>
+							<br />
+							<table>
+								<tbody>
+									{wordListAlpha.slice(5, 10).map((item, i) => {
+										return (
+											<tr key={i}>
+												<td>{item && item.word}</td>
+												<td>{item && item.mean}</td>
+											</tr>
+										)
+									})}
+								</tbody>
+							</table>
+						</>
 					)}
 				</div>
 				<img
