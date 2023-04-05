caaa5def22a769d3060a449f16c4d033ab34f75e
dnjs0236 <dnjs0236@gmail.com>
2023-04-05 13:10:16 +0900

    [EDIT] not found 페이지 수정

--- frontend/frontproject/src/pages/NotFound.tsx
+++ frontend/frontproject/src/pages/NotFound.tsx
@@ -1,35 +1,22 @@
-import { Link } from "react-router-dom";
-import styles from "../assets/css/main.module.css";
+import { Link, useNavigate } from 'react-router-dom'
+import styles from '../assets/css/NotFound.module.css'
 
 const NotFound = () => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.picstory}>
-				<h1>
-					<span>P</span>
-					<span>a</span>
-					<span>g</span>
-					<span>e</span>
-					<span>N</span>
-					<span>o</span>
-					<span>t</span>
-					<span>F</span>
-					<span>o</span>
-					<span>u</span>
-					<span>n</span>
-					<span>d</span>
-				</h1>
-			</div>
-			<div className={styles.clear}></div>
-			<Link to='/'>
-				<button className={styles.btn1}>HOME</button>
-			</Link>
+  const navigate = useNavigate()
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}> Page Not Found</div>
+      <div className={styles.text2}>요청하신 페이지를 찾을 수 없습니다.</div>
+      <button
+        className={styles.homeBtn}
+        onClick={() => {
+          navigate('/')
+        }}
+      >
+        HOME
+      </button>
+    </div>
+  )
+}
 
-			<h3 style={{ color: "white", margin: "2em" }}>
-				죄송합니다. 요청하신 페이지를 찾을 수 없습니다.
-			</h3>
-		</div>
-	);
-};
-
-export default NotFound;
+export default NotFound
